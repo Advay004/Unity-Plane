046f2835fa6a41c56e5a66e6b3d5f4ac8d7003f4
Advay004 <136166281+Advay004@users.noreply.github.com>
2024-08-01 10:46:54 +0300

    added scripts

---
+++ PlayerControll.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed=20.0f;
+    public float rotationSpeed;
+    public float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward *Time.deltaTime* speed);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.left * rotationSpeed * Time.deltaTime*verticalInput);
+    }
+}
---
+++ PropRotate.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropRotate : MonoBehaviour
+{
+    public float speed=20.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.RotateAround(Vector3.one, Time.deltaTime * speed);
+    }
+}
